ddf5d7bc6938f7a04f07f21a0b2087c04bcbacb4
duong tam kien <tk@deveha.com>
2013-01-22 18:16:09 +0100

    clean html title display

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title>cortext <?php wp_title( ' ● ', true, 'left' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
