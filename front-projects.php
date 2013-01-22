4d3069f5fd3505e7a7c15237aa6065e3585557a0
duong tam kien <tk@deveha.com>
2013-01-22 18:11:34 +0100

    blah

--- front-projects.php
+++ front-projects.php
@@ -1,5 +1,5 @@
 <div class="project">
-	<div class="thumbnail">	<?php the_post_thumbnail('thumbnail'); ?></div>
+	<div class="thumbnail">	<?php the_post_thumbnail('loop-front'); ?></div>
 	<h4><a href="<? the_permalink() ?>"><? the_title() ?></a></h4>
 	<div class="excerpt"><? the_excerpt() ?></div>
 </div>
