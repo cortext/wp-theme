3bf7f80e68bb7c0b70d5191dd77c7db3542d029b
duong tam kien <tk@deveha.com>
2013-01-22 18:09:42 +0100

    fix thumbnail sizes

--- front-projects.php
+++ front-projects.php
@@ -1,5 +1,5 @@
 <div class="project">
-	<div class="thumbnail">	<?php the_post_thumbnail(); ?></div>
+	<div class="thumbnail">	<?php the_post_thumbnail('thumbnail'); ?></div>
 	<h4><a href="<? the_permalink() ?>"><? the_title() ?></a></h4>
 	<div class="excerpt"><? the_excerpt() ?></div>
 </div>
